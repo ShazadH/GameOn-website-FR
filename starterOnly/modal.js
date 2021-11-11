03bdcd95e3113aa4c33bf07bc6be87f848969dde
Shazad Hoang <shazad.hoang@gmail.com>
2021-11-11 15:38:16 +0100

    Add first and last name verif + email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,19 +1,15 @@
-function editNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close"); // Select the close class
-const firstNameInput = document.getElementById("firstName");
 const submitBtn = document.querySelector(".btn-submit");
+const firstName = document.getElementById("firstName");
+const firstNameError = document.querySelector(".firstNameError");
+const lastName = document.getElementById("lastName");
+const lastNameError = document.querySelector(".lastNameError");
+const email = document.getElementById("email");
+const emailError = document.querySelector(".emailError");
 
 //glogal var
 let formError = false;
@@ -24,13 +20,33 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // close modal event
 modalClose.addEventListener("click", closeModal);
 
+// All Check on submit button click
+
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     formError = false;
+    //Check all input
     checkFirstNameValidity();
+    checkLasrNameValidity();
+    checkEmailValidity();
+
+    if (formError === false) {
+        // close modal + succes message
+    }
 });
 
+////////////////////////
 ////// Functions ///////
+////////////////////////
+
+function editNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
 // launch modal form
 function launchModal() {
@@ -44,14 +60,50 @@ function closeModal() {
 
 // check first name
 function checkFirstNameValidity() {
-    const regex = new RegExp("[a-zA-Z]{2,15}");
-    const value = firstNameInput.value;
+    const regex = new RegExp("^[a-zA-Z]{2,15}$");
+    const value = firstName.value;
 
-    if (regex.test(value)) {
-        console.log("Pass");
-    } else {
-        console.log("Reject");
+    if (regex.test(value) === false) {
         formError = true;
-        //firstNameInput.dataset["error"] = true;
+        firstNameError.style.display = "inline";
+        firstName.parentElement.dataset.errorVisible = "true";
+        console.log("first name", value);
+    } else {
+        firstNameError.style.display = "none";
+        firstName.parentElement.dataset.errorVisible = "false";
+    }
+}
+
+//check last name
+function checkLasrNameValidity() {
+    const regex = new RegExp("[a-zA-Z]{2,15}");
+    const value = lastName.value;
+
+    if (regex.test(value) === false) {
+        formError = true;
+        lastNameError.style.display = "inline";
+        lastName.parentElement.dataset.errorVisible = "true";
+        console.log("last name", value);
+    } else {
+        lastNameError.style.display = "none";
+        lastName.parentElement.dataset.errorVisible = "false";
+    }
+}
+
+//Check email
+
+function checkEmailValidity() {
+    const regex = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
+    const value = email.value;
+
+    if (regex.test(value) === false) {
+        console.log("false", value);
+        formError = true;
+        emailError.style.display = "inline";
+        email.parentElement.dataset.errorVisible = "true";
+    } else {
+        console.log("true", value);
+        emailError.style.display = "none";
+        email.parentElement.dataset.errorVisible = "false";
     }
 }
